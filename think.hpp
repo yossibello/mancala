64ded23d089b2e9dedc854b0e101623e1b29a44a
Bello <yosef.bello@intel.com>
2019-12-29 18:26:51 +0200

    return max score

--- think.hpp
+++ think.hpp
@@ -1,10 +1,16 @@
-think(board b, __int8 my_side)
+int think(board b, int level)
 {
 	if (b.end())
-		reurn ???
-		
+		return b.score();
+	if (level == 0)
+		return b.score();
+	
+	int max_score = 0;
+	
 	for (int i = my_side*7; i < 6+my_side*7; i++)
-		think(b.move(i), 1-my_side);
-		
+		max_score = max(max_score, think(b.move(i), level--));
+	
+	return max_score; 
 }
+
 	
