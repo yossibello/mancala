d8e3f4bfd2adb236ff433f6a95107b55a2005760
Bello <yosef.bello@intel.com>
2019-12-30 09:37:18 +0200

    use level to idicate turn

--- think.hpp
+++ think.hpp
@@ -7,7 +7,7 @@ int think(board b, int level)
 	
 	int max_score = 0;
 	
-	for (int i = my_side*7; i < 6+my_side*7; i++)
+	for (int i = level%2*7; i < 6+level%2*7; i++)
 		max_score = max(max_score, think(b.move(i), level--));
 	
 	return max_score; 
